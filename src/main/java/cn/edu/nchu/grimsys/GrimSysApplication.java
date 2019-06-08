fc3d56d4cf77403fd4eb852467b02bcff0f7b96a
16201535wangshenyu <2697431730@qq.com>
2019-06-08 16:39:03 +0800

    WEB高级编程综合课程设计模板

---
+++ src/main/java/cn/edu/nchu/grimsys/GrimSysApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.nchu.grimsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrimSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrimSysApplication.class, args);
+    }
+
+}
